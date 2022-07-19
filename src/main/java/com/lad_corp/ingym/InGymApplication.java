dd391623320d0c66ff06122715f1b3c33e48bf64
Rafael Pereira <rafaelpereira901@gmail.com>
2022-07-19 20:55:54 -0300

    initial commit

---
+++ src/main/java/com/lad_corp/ingym/InGymApplication.java
@@ -0,0 +1,23 @@
+package com.lad_corp.ingym;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@SpringBootApplication
+public class InGymApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InGymApplication.class, args);
+	}
+	
+	@CrossOrigin
+	@RequestMapping("/status")
+	public ResponseEntity<String> home() {
+		return new ResponseEntity<>("Status: OK" , HttpStatus.OK);
+	}
+
+}
