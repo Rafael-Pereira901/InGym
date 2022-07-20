026f91917532860811abd2a7e8486f8ad8bc610c
Rafael Pereira <rafaelpereira901@gmail.com>
2022-07-19 21:24:25 -0300

    Added annotation

--- src/main/java/com/lad_corp/ingym/InGymApplication.java
+++ src/main/java/com/lad_corp/ingym/InGymApplication.java
@@ -6,8 +6,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class InGymApplication {
 
 	public static void main(String[] args) {
